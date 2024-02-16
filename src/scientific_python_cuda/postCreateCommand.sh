ed2eff4c5d996db2de451a717bd5f09ce6d4c761
lcharleux <ludovic.charleux@univ-smb.fr>
2024-02-16 11:42:55 +0100

    Add Dockerfile and build scripts for scientific Python with CUDA

---
+++ src/scientific_python_cuda/postCreateCommand.sh
@@ -0,0 +1,12 @@
+# sh -c "$(curl -sS https://starship.rs/install.sh)" -y -f
+#yes | curl -sS https://starship.rs/install.sh | sh
+
+
+# echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
+# echo "conda activate $CONDA_ENV_NAME" >> ~/.bashrc
+# echo 'eval "$(starship init bash)"' >> ~/.bashrc
+# echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
+echo "#######################################################"
+echo ${localWorkspaceFolder}
+echo "#######################################################"

6bb90eb593c2ede0734ebcc7d9923692e9629d6e
emileroux83 <emile.roux83@gmail.com>
2024-03-07 15:46:57 +0100

    maj wsl image

---
+++ src/scientific_python/.devcontainer/postStartCommand.sh
@@ -0,0 +1,5 @@
+echo "#######################################################"
+echo "PostStartCommand:"
+echo "#######################################################"
+
+/opt/mamba/envs/${CONDA_ENV_NAME}/bin/pre-commit install
